7cd69af2cd12810a34ac782d290ed264f4d4f6fc
vynu <vynu@live.com>
2017-07-23 12:06:43 -0400

    first commit of sbt-scala

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "Pilot-sbt-scala"
+
+version := "1.0"
+
+scalaVersion := "2.12.2"
+        
---
+++ src/main/scala/TestScala/TestHellow.scala
@@ -0,0 +1,9 @@
+package TestScala
+
+object TestHellow {
+
+  def main(args: Array[String]): Unit = {
+    print("Hello this is test SBT scala project!")
+  }
+
+}
